878603712e31c04287dba6dcf9aeabe3e2bbbd79
arunprasadh7 <m.arunprasadh96@gmail.com>
2023-09-15 19:11:16 +0530

    on delete

---
+++ 13 ON DELETE.sql
@@ -0,0 +1,17 @@
+-- 13 ON DELETE
+
+CREATE TABLE branch (
+  branch_id INT PRIMARY KEY,
+  branch_name VARCHAR(40),
+  mgr_id INT,
+  mgr_start_date DATE,
+  FOREIGN KEY(mgr_id) REFERENCES employee(emp_id) ON DELETE SET NULL -- mgr_id is a foreign key in branch table, not crucial
+);
+
+CREATE TABLE branch_supplier (
+  branch_id INT,
+  supplier_name VARCHAR(40),
+  supply_type VARCHAR(40),
+  PRIMARY KEY(branch_id, supplier_name),
+  FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE CASCADE -- branch_id is primary key and crucial, set null cant be used
+);
