5a668ed56cfedbf3f9d48044ad3732e1f5968b9d
arunprasadh7 <m.arunprasadh96@gmail.com>
2023-09-15 19:10:56 +0530

    nested query

---
+++ 12 NESTED QUERIES.sql
@@ -0,0 +1,79 @@
+-- 12 NESTED QUERIES
+
+-- Find names of all employees who have sold over 50,000
+SELECT employee.first_name, employee.last_name
+FROM employee
+WHERE employee.emp_id IN (
+	SELECT emp_id 
+	FROM works_with
+	WHERE total_sales > 50000
+);
+
+-- Find names of all employees who have sold over 30,000
+SELECT employee.first_name, employee.last_name
+FROM employee
+WHERE employee.emp_id IN (
+	SELECT works_with.emp_id
+	FROM works_with
+	WHERE works_with.total_sales > 30000
+);
+
+-- Find all clients who are handled by the branch that Michael Scott manages
+-- Assume you know Michael's ID
+SELECT client.client_name
+FROM client
+WHERE client.branch_id IN (
+	SELECT branch.branch_id
+	FROM branch
+	WHERE branch.mgr_id = 102
+);
+
+-- Find all clients who are handles by the branch that Michael Scott manages
+ -- Assume you DONT'T know Michael's ID
+SELECT client.client_name
+FROM client 
+WHERE client.branch_id IN (
+	SELECT branch.branch_id
+	FROM branch 
+	WHERE branch.mgr_id IN (
+		SELECT employee.emp_id
+		FROM employee
+		WHERE employee.first_name LIKE('Michael')
+    )
+    );
+    
+SELECT client.client_id, client.client_name
+ FROM client
+ WHERE client.branch_id = (SELECT branch.branch_id
+                           FROM branch
+                           WHERE branch.mgr_id = (SELECT employee.emp_id
+                                                  FROM employee
+                                                  WHERE employee.first_name = 'Michael' AND employee.last_name ='Scott'
+                                                  LIMIT 1));
+                                                  
+-- Find the names of employees who work with clients handled by the scranton branch
+   
+SELECT employee.first_name, employee.last_name
+FROM employee
+WHERE employee.emp_id IN (
+						SELECT works_with.emp_id
+FROM works_with
+WHERE works_with.client_id IN (
+SELECT client.client_id
+FROM client
+WHERE client.branch_id = (
+	SELECT branch.branch_id
+	FROM branch
+	WHERE branch.branch_name = 'Scranton')
+    )
+    );
+    
+-- Find the names of all clients who have spent more than 100,000 dollars
+SELECT client.client_name
+FROM client
+WHERE client.client_id IN (
+SELECT  works_with.client_id
+FROM works_with
+GROUP BY works_with.client_id
+HAVING SUM(works_with.total_sales) > 100000
+);
